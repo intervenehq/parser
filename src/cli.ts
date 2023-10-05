c6c08462fd233258627aac7920ceaeee26029bf3
Rohan Mayya <rohan.mayya@gmail.com>
2023-10-05 17:13:00 +0530

    cli.ts first pass

---
+++ src/cli.ts
@@ -0,0 +1,19 @@
+import { program } from "commander";
+
+program
+  .name("intervene-cli")
+  .description("CLI for Intervene to parse natural language")
+  .version("0.0.1");
+
+program
+  .command("split")
+  .description("Split a string into substrings and display as an array")
+  .argument("<string>", "string to split")
+  .option("--first", "display just the first substring")
+  .option("-s, --separator <char>", "separator character", ",")
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
+
+program.parse();
